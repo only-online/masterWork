d45798df8c8a4cb7fe836d82592c5315edf4e0d5
online <sanzhar1992@gmail.com>
2018-03-18 03:53:18 +0600

    верстка 0.2 версия

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,13 @@ router.get('/single', function(req, res, next) {
     res.render('single', { title: 'Sanzhar' });
 });
 
+router.get('/archive', function(req, res, next) {
+    res.render('archive', { title: 'Sanzhar' });
+});
+
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'Sanzhar' });
+});
+
 
 module.exports = router;
